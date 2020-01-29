279e1d21735632a29b8bf6abf703b3c1ef2768ae
Ruslan Stoyanov <ep8kha@gmail.com>
2020-01-29 09:48:18 +0500

    ram

--- memory.sv
+++ memory.sv
@@ -8,10 +8,10 @@ input wire clk,rst,ena,we,re;
 output wire[WIDTH-1:0] q;
 
 wire ena_delay;
-d_ff_wide #(1) ena_delay_ff(.d(ena),.clk(clk),.rst(!ena),.ena(ena),.q(ena_delay));
+d_ff_wide #(1) ena_delay_ff(.d(ena),.clk(clk),.rst(rst),.ena(1'b1),.q(ena_delay));
 
-d_ff_wide #(WIDTH) ssram_ff (.d(d),.clk(clk),.rst(rst),.ena(ena_delay & we),.q(q));
-buffer_z #(WIDTH) ssram_buffer (.ena(ena_delay & re),.d(q),.q(d));
+d_ff_wide #(WIDTH) ssram_ff (.d(d),.clk(clk),.rst(rst),.ena(ena_delay & ena & we),.q(q));
+buffer_z #(WIDTH) ssram_buffer (.ena(ena_delay & ena & re),.d(q),.q(d));
 endmodule
 
 module ssram_256 #(parameter WIDTH=1,DEPTH=1) (clk,rst,we,re,row,column,data,out);
