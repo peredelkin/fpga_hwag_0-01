a9dfc47f28c32fb712b7b1d17ae0e04f25a99b06
Ruslan <ep8kha@gmail.com>
2020-04-03 00:38:58 +0500

    поиск метки

--- hwag.sv
+++ hwag.sv
@@ -12,13 +12,12 @@
 `include "bit_operation.sv"
 `include "math.sv"
 
-module hwag(clk,cap_in,cap_out);
+module hwag(clk,cap_in,cap_out,gap_out);
 input wire clk;
 input wire cap_in;
 output wire cap_out;
 
 wire edge0,edge1;
-
 capture_flt_edge_det_sel #(4,10) vr_filter 
 										(	.d(cap_in),
 											.clk(clk),
@@ -33,7 +32,33 @@ capture_flt_edge_det_sel #(4,10) vr_filter
 											.edge0(edge0),
 											.edge1(edge1));
 
-endmodule
+wire [23:0] pcnt_out;											
+counter_compare #(24) pcnt 	(	.clk(clk),
+											.ena(pcnt_ne_top),
+											.rst(rst),
+											.srst(edge1),
+											.sload(1'b0),
+											.dload(23'd0),
+											.dout(pcnt_out),
+											.dtop(24'hFFFFFF),
+											.out_e_top(pcnt_e_top),
+											.out_ne_top(pcnt_ne_top));
 
+wire [23:0] pcnt0_out,pcnt1_out,pcnt2_out;
+period_capture_3 #(24) pcnt_cap (.d(pcnt_out),
+											.clk(clk),
+											.rst(rst | pcnt_e_top),
+											.ena(edge0),
+											.q0(pcnt0_out),
+											.q1(pcnt1_out),
+											.q2(pcnt2_out));
+
+output wire gap_out;
+gap_search #(24) gap_start_srch (.cap0(pcnt0_out),
+											.cap1(pcnt1_out),
+											.cap2(pcnt2_out),
+											.gap(gap_out));
+
+endmodule
 //`endif
 
